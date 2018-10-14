6eb3cf10cd612ea1000884f917a13d04201bd7f6
Dimitri C <couwenberg@gmail.com>
2018-10-14 20:01:39 +1100

    update

---
+++ src/Button2.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Button2 = styled.button`
+  color: red;
+`
---
+++ src/Button2.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
+
+
+import Button2 from './Button2'
+
+test('it works', () => {
+    const tree = renderer.create(<Button2 />).toJSON()
+    expect(tree).toMatchSnapshot()
+    expect(tree).toHaveStyleRule('color', 'red')
+})
